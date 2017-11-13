eb8fa68c4c39fc7515ca2a1fcc6c90ff2924faf6
Chip Bell <cbell@pbs.org>
2017-11-13 16:01:17 -0500

    More code stubbing

---
+++ src/ios/HideHomeIndicator.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDV.h>
+
+@interface HideHomeIndicatorPlugin : CDVPlugin {
+}
+
+@end
---
+++ src/ios/HideHomeIndicator.m
@@ -0,0 +1,11 @@
+#import "HideHomeIndicator.h"
+
+@implementation HideHomeIndicatorPlugin
+
+- (void) pluginInitialize
+{
+    NSLog(@"Greetings from the plugin y'all");
+}
+
+@end
+
